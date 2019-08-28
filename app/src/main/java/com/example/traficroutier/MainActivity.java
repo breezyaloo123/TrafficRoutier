b8ccfe03fd06a1ff788a902a3df2ce51db121311
breezyaloo123 <alassanesall771@gmail.com>
2019-08-28 02:54:31 +0100

    modify mainActivity

--- app/src/main/java/com/example/traficroutier/MainActivity.java
+++ app/src/main/java/com/example/traficroutier/MainActivity.java
@@ -1,18 +1,11 @@
 package com.example.traficroutier;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
@@ -21,23 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button=findViewById(R.id.inscription);
-        //recuperer l'ID de l'utilisteur
-
-        String id= Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
-
-        //Create the Map
-        Map<String,String> value = new HashMap<>();
-        value.put("longitude","12");
-        value.put("latittude","9");
-        value.put("vitesse","23");
-
-
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("utilisateur");
-        //push() method will automaticly generate an id
-        myRef.push().setValue(id);
-
         //When we create click on this button an other activity will come
         button.setOnClickListener(new View.OnClickListener() {
             @Override
