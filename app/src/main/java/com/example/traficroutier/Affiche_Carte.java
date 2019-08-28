ff8a34fbb95ae8519332956f3a80cddf3ad4f397
breezyaloo123 <alassanesall771@gmail.com>
2019-08-28 03:00:59 +0100

    new Activity

---
+++ app/src/main/java/com/example/traficroutier/Affiche_Carte.java
@@ -0,0 +1,14 @@
+package com.example.traficroutier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Affiche_Carte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affiche__carte);
+    }
+}
